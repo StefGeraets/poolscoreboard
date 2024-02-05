fbad9bbe865ac6f683046b0d17837a3e812fbc32
Stef Geraets <stefgeraets@Stefs-MacBook-Pro.local>
2024-02-05 22:36:07 +0100

    Feat: Create and Delete team actions

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,6 @@
+import { fail, type Actions } from '@sveltejs/kit';
 import DB from '../lib';
-import type { PageServerLoad } from './$types';
+import type { Action, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	const players = await DB.player.findMany({
@@ -21,4 +22,25 @@ export const load: PageServerLoad = async () => {
 	});
 
 	return { players, teams, matches };
+};
+
+export const actions: Actions = {
+	createTeam: async ({ request }) => {
+		const data = await request.formData();
+		const name = String(data.get('name'));
+
+		if (!name) {
+			return fail(400, { name, missing: true });
+		}
+
+		await DB.team.create({
+			data: { name: name as string }
+		});
+	},
+	deleteTeam: async ({ request }) => {
+		const data = await request.formData();
+		const id = Number(data.get('id'));
+
+		await DB.team.delete({ where: { id } });
+	}
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
+  import { enhance } from '$app/forms';
+  let showTeamForm: boolean = false;
+  const toggleTeamForm = () => {
+    showTeamForm = !showTeamForm;
+  }
   export let data;
+  export let form;
 </script>
 
 <header class="text-center text-2xl bg-gray-950 text-blue-100 py-5">
@@ -8,10 +14,24 @@
 
 <div class="container mx-auto grid grid-cols-7 gap-4">
   <section class="bg-gray-900 rounded-lg border border-gray-800 p-4 col-span-2 tabular-nums">
-    <h2 class="uppercase text-lg font-bold mb-4">Team Scores</h2>
+    <header class="flex justify-between align-middle">
+      <h2 class="uppercase text-lg font-bold mb-4">Team Scores</h2>
+      <button 
+        class="rounded h-7 w-7 flex justify-center items-center bg-gray-800 border border-gray-700 hover:bg-gray-700"
+        on:click={toggleTeamForm}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-plus" width="24" height="24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
+      </button>
+    </header>
+    {#if showTeamForm}
+      <form method="POST" action="?/createTeam" use:enhance>
+        <input type="text" name="name" id="name">
+        <button type="submit">Save</button>
+      </form>
+    {/if}
     {#each data.teams as team, index }
       <div 
-        class="grid grid-cols-3 items-center w-full border-gray-800 py-2"
+        class="grid grid-cols-3 items-center w-full border-gray-800 py-2 group"
         class:border-b={index + 1 !== data.teams.length}
       >
         <div>
@@ -24,6 +44,13 @@
         </div>
         <div class="text-center text-sm">{team.players.length} players</div>
         <div class="text-end">{team.score}</div>
+        <div class="hidden group-hover:block">
+          <button>Edit</button>
+          <form method="POST" action="?/deleteTeam" use:enhance>
+            <input type="hidden" name="id" value={team.id}>
+            <button type="submit">Delete</button>
+          </form>
+        </div>
       </div>
     {/each}
   </section>
