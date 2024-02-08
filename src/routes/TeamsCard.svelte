b57b496856be3d3796e314fe6e285ef0b852598f
Stef Geraets <stef.geraets@eventix.nl>
2024-02-08 16:23:31 +0100

    Feat: Remove team edit

--- src/routes/TeamsCard.svelte
+++ src/routes/TeamsCard.svelte
@@ -102,33 +102,35 @@
         {#if index + 1 === 1 && team.score !== 0}
           🏆
         {/if}
-        <span class="font-bold">{team.name}</span>
+        <span class="font-bold">{team.name} {team.id}</span>
       </div>
       <div class="text-center text-sm">{team.players.length} players</div>
       <div class="text-end">{team.score}</div>
-      <div 
-        class="
-          flex right-0 absolute gap-2 bg-transparent translate-x-full
-          group-hover:translate-x-0 group-hover:bg-gray-900
-          transition-all ease-in-out duration-300
-          "
-      >
-        <button 
-          class="hover:text-blue-400"
-          on:click={() => {
-            teamId = team.id,
-            teamName = team.name
-            teamEditDialog.showModal();
-          }}
+      {#if false }
+        <div 
+          class="
+            flex right-0 absolute gap-2 bg-transparent translate-x-full
+            group-hover:translate-x-0 group-hover:bg-gray-900
+            transition-all ease-in-out duration-300
+            "
         >
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
-        </button>
-        <form method="POST" action="?/deleteTeam" use:enhance>
-          <input type="hidden" name="id" value={team.id}>
-          <button type="submit" class="hover:text-red-400">
-            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg></button>
-        </form>
-      </div>
+          <button 
+            class="hover:text-blue-400"
+            on:click={() => {
+              teamId = team.id,
+              teamName = team.name
+              teamEditDialog.showModal();
+            }}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1" /><path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z" /><path d="M16 5l3 3" /></svg>
+          </button>
+          <form method="POST" action="?/deleteTeam" use:enhance>
+            <input type="hidden" name="id" value={team.id}>
+            <button type="submit" class="hover:text-red-400">
+              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg></button>
+          </form>
+        </div>
+      {/if}
     </div>
   {/each}
 </Card>
