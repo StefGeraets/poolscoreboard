ee44a7f303c5f1b428c7edbd08eb0a0d976d2b02
Stef Geraets <stef.geraets@eventix.nl>
2024-02-09 08:22:10 +0100

    Promise all and loading icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,19 +13,27 @@
 </header>
 
 <div class="container mx-auto grid lg:grid-cols-7 md:grid-cols-3 grid-cols-1 gap-4">
-  {#await data.teams}
-    Loading
-  {:then teams} 
+  {#await Promise.all([data.teams, data.players, data.matches])}
+  <div class="lg:col-span-7 md:col-span-3">
+    <svg 
+      xmlns="http://www.w3.org/2000/svg"
+      class="animate-spin w-20 h-20 mx-auto" 
+      viewBox="0 0 24 24" 
+      stroke-width="1.25" 
+      stroke="currentColor" 
+      fill="none" 
+      stroke-linecap="round" 
+      stroke-linejoin="round"
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M12 10m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+      <path d="M12 14m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+      <path d="M12 12m-8 0a8 8 0 1 0 16 0a8 8 0 1 0 -16 0" />
+    </svg>
+  </div>
+  {:then [teams, players, matches]} 
     <TeamsCard teamData={teams} {form} class="order-3 md:order-1"/>
-  {/await}
-  {#await Promise.all([data.players, data.teams])}
-    Loading
-  {:then [players, teams]} 
     <PlayerCard playerData={players} teamData={teams} {form} class="order-2 md:order-2"/>
-  {/await}
-  {#await Promise.all([data.players, data.matches])}
-    Loading
-  {:then [players, matches]} 
     <MatchesCard playerData={players} matchData={matches} {form} class="order-1 md:order-3"/>
   {/await}
 </div>
