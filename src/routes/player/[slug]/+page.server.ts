6f1f520e978af0e660776e64a4af8a609bb0a23c
Stef Geraets <stef.geraets@eventix.nl>
2024-03-14 09:20:45 +0100

    Feat: Update to real stats

--- src/routes/player/[slug]/+page.server.ts
+++ src/routes/player/[slug]/+page.server.ts
@@ -5,11 +5,15 @@ import type { PageServerLoad } from "./$types";
 export const load: PageServerLoad = async ({ params, fetch }) => {
   const name = params.slug
   const player = await DB.player.findUnique({where: {name}})
+  const statsRes = await fetch(`/api/winsLossess`).then((r) => r.json());
 
   if(!player) return redirect(301, '/')
 
+  const stats = statsRes.matchesPerPlayer.find((stat) => stat.id === player.id);
+
   return {
     player,
+    stats,
     matches: await fetch(`/api/player/${player.id}/matches`).then((r) => r.json())
   }
 }
--- src/routes/player/[slug]/+page.svelte
+++ src/routes/player/[slug]/+page.svelte
@@ -8,29 +8,29 @@
   <h1 class="text-2xl font-black">{data.player.name}</h1>
 </header>
 
-<div class="container px-32 mx-auto grid grid-cols-4 gap-4 justify-items-center">
+<div class="container lg:px-32 px-1 md:px-4 mx-auto grid grid-cols-2 md:grid-cols-4 gap-1 md:gap-4 justify-items-stretch">
   <div class="flex flex-col items-center gap-1 bg-gray-900 rounded-lg border border-gray-800 py-4 px-8 md:col-span-1 tabular-nums self-start">
     <h2 class="uppercase text-green-400">Wins</h2>
-    <p class="text-3xl font-black">{data.player.wins}</p>
+    <p class="text-3xl font-black">{data.stats.wins}</p>
   </div>
   <div class="flex flex-col items-center gap-1 bg-gray-900 rounded-lg border border-gray-800 py-4 px-8 md:col-span-1 tabular-nums self-start">
     <h2 class="uppercase text-red-500">Lossess</h2>
-    <p class="text-3xl font-black ">{data.player.wins}</p>
+    <p class="text-3xl font-black ">{data.stats.lossess}</p>
   </div>
   <div class="flex flex-col items-center gap-1 bg-gray-900 rounded-lg border border-gray-800 py-4 px-8 md:col-span-1 tabular-nums self-start">
-    <h2 class="uppercase text-blue-400">Games Played</h2>
-    <p class="text-3xl font-black">{data.player.wins}</p>
+    <h2 class="uppercase text-blue-400">Total</h2>
+    <p class="text-3xl font-black">{data.stats.amountOfGames}</p>
   </div>
   <div class="flex flex-col items-center gap-1 bg-gray-900 rounded-lg border border-gray-800 py-4 px-8 md:col-span-1 tabular-nums self-start">
     <h2 class="uppercase">K/D</h2>
-    <p class="text-3xl font-black">{data.player.wins}</p>
+    <p class="text-3xl font-black">{data.stats.kd}</p>
   </div>
 </div>
 
 <div class="text-center text-2xl bg-gray-950 text-blue-100 py-5">
   <h3 class="text-2xl font-black">Played Games</h3>
 </div>
-<div class="grid grid-cols-5 gap-2 container mx-auto">
+<div class="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-2 px-1 md:px-2 lg:px-4 container mx-auto">
   {#each data.matches as match }
     <div class="flex gap-2 justify-between bg-gray-900 rounded-lg border border-gray-800 py-2 px-4">
       <div>
