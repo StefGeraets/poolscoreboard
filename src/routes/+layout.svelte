ae3c6e8d42c6b24c9346ec4abb99c9e572a31b09
Stef Geraets <stef.geraets@eventix.nl>
2024-04-18 08:14:06 +0200

    Feat: Install Insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-  import "../satoshi.css";
-  import "../app.css";
+	import '../satoshi.css';
+	import '../app.css';
+
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectSpeedInsights();
 </script>
 
 <main class="bg-gray-950">
-  <slot />
+	<slot />
 </main>
