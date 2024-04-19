05898730331429bd5036e01263fae40ab64a7489
Stef Geraets <stef.geraets@eventix.nl>
2024-04-19 14:28:33 +0200

    Fix: Filter out S1 matches

--- src/lib/db/fetches.ts
+++ src/lib/db/fetches.ts
@@ -1,15 +1,18 @@
 import DB from '$lib';
 
 export const players = async () => {
-  return await DB.player.findMany({
-    orderBy: [{wins: 'desc'}],
-  })
+	return await DB.player.findMany({
+		orderBy: [{ wins: 'desc' }]
+	});
 };
 export const teams = async () => {
-  return await DB.team.findMany({
-  orderBy: [{score: 'desc'}],
-})};
+	return await DB.team.findMany({
+		orderBy: [{ score: 'desc' }]
+	});
+};
 export const matches = async () => {
-  return await DB.match.findMany({
-  orderBy: [{createdAt: 'desc'}]
-})};
+	return await DB.match.findMany({
+		orderBy: [{ createdAt: 'desc' }],
+		where: { s1: false }
+	});
+};
