4bc8663d074d439bdfaa247e3e29728328c25afc
Stef Geraets <stef.geraets@eventix.nl>
2024-04-26 17:49:21 +0200

    Fix: Vercel logs

--- src/routes/api/players/+server.ts
+++ src/routes/api/players/+server.ts
@@ -4,100 +4,104 @@ import type { RequestHandler } from '@sveltejs/kit';
 import type { RankedPlayer } from '$lib/utils/playerHelpers';
 
 export const GET: RequestHandler = async ({ url }) => {
-	const players = await DB.player.findMany({
-		orderBy: [
-			{
-				s1_score: 'desc'
-			},
-			{
-				s1_wins: 'desc'
-			},
-			{
-				s1_totalGames: 'desc'
-			},
-			{
-				name: 'asc'
-			}
-		]
-	});
-
-	const dailyRankedPlayers = players
-		.filter((p) => p.s1_ranked)
-		.toSorted((p1, p2) => {
-			if (p1.s1_dayScore < p2.s1_dayScore) return 1;
-			if (p1.s1_dayScore > p2.s1_dayScore) return -1;
-
-			if (p1.s1_wins < p2.s1_wins) return 1;
-			if (p1.s1_wins > p2.s1_wins) return -1;
-
-			if (p1.s1_totalGames < p2.s1_totalGames) return 1;
-			if (p1.s1_totalGames > p2.s1_totalGames) return -1;
-
-			if (p1.name > p2.name) return 1;
-			if (p1.name < p2.name) return -1;
-
-			return 0;
+	try {
+		const players = await DB.player.findMany({
+			orderBy: [
+				{
+					s1_score: 'desc'
+				},
+				{
+					s1_wins: 'desc'
+				},
+				{
+					s1_totalGames: 'desc'
+				},
+				{
+					name: 'asc'
+				}
+			]
 		});
 
-	const weeklyRankedPlayers = players
-		.filter((p) => p.s1_ranked)
-		.toSorted((p1, p2) => {
-			if (p1.s1_weekScore < p2.s1_weekScore) return 1;
-			if (p1.s1_weekScore > p2.s1_weekScore) return -1;
+		const dailyRankedPlayers = players
+			.filter((p) => p.s1_ranked)
+			.toSorted((p1, p2) => {
+				if (p1.s1_dayScore < p2.s1_dayScore) return 1;
+				if (p1.s1_dayScore > p2.s1_dayScore) return -1;
 
-			if (p1.s1_wins < p2.s1_wins) return 1;
-			if (p1.s1_wins > p2.s1_wins) return -1;
+				if (p1.s1_wins < p2.s1_wins) return 1;
+				if (p1.s1_wins > p2.s1_wins) return -1;
 
-			if (p1.s1_totalGames < p2.s1_totalGames) return 1;
-			if (p1.s1_totalGames > p2.s1_totalGames) return -1;
+				if (p1.s1_totalGames < p2.s1_totalGames) return 1;
+				if (p1.s1_totalGames > p2.s1_totalGames) return -1;
 
-			if (p1.name > p2.name) return 1;
-			if (p1.name < p2.name) return -1;
+				if (p1.name > p2.name) return 1;
+				if (p1.name < p2.name) return -1;
 
-			return 0;
+				return 0;
+			});
+
+		const weeklyRankedPlayers = players
+			.filter((p) => p.s1_ranked)
+			.toSorted((p1, p2) => {
+				if (p1.s1_weekScore < p2.s1_weekScore) return 1;
+				if (p1.s1_weekScore > p2.s1_weekScore) return -1;
+
+				if (p1.s1_wins < p2.s1_wins) return 1;
+				if (p1.s1_wins > p2.s1_wins) return -1;
+
+				if (p1.s1_totalGames < p2.s1_totalGames) return 1;
+				if (p1.s1_totalGames > p2.s1_totalGames) return -1;
+
+				if (p1.name > p2.name) return 1;
+				if (p1.name < p2.name) return -1;
+
+				return 0;
+			});
+		const monthlyRankedPlayers = players
+			.filter((p) => p.s1_ranked)
+			.toSorted((p1, p2) => {
+				if (p1.s1_monthScore < p2.s1_monthScore) return 1;
+				if (p1.s1_monthScore > p2.s1_monthScore) return -1;
+
+				if (p1.s1_wins < p2.s1_wins) return 1;
+				if (p1.s1_wins > p2.s1_wins) return -1;
+
+				if (p1.s1_totalGames < p2.s1_totalGames) return 1;
+				if (p1.s1_totalGames > p2.s1_totalGames) return -1;
+
+				if (p1.name > p2.name) return 1;
+				if (p1.name < p2.name) return -1;
+
+				return 0;
+			});
+
+		const playersWithRanking: RankedPlayer[] = players.map((player, i) => {
+			const currRank = player.s1_ranked ? i + 1 : 0;
+			const dailyRanking = dailyRankedPlayers.findIndex((p) => p.id === player.id) + 1;
+			const weeklyRanking = weeklyRankedPlayers.findIndex((p) => p.id === player.id) + 1;
+			const monthlyRanking = monthlyRankedPlayers.findIndex((p) => p.id === player.id) + 1;
+
+			return {
+				...player,
+				dailyRank: dailyRanking - currRank,
+				weeklyRank: weeklyRanking - currRank,
+				monthlyRank: monthlyRanking - currRank
+			};
 		});
-	const monthlyRankedPlayers = players
-		.filter((p) => p.s1_ranked)
-		.toSorted((p1, p2) => {
-			if (p1.s1_monthScore < p2.s1_monthScore) return 1;
-			if (p1.s1_monthScore > p2.s1_monthScore) return -1;
 
-			if (p1.s1_wins < p2.s1_wins) return 1;
-			if (p1.s1_wins > p2.s1_wins) return -1;
+		let result: Partial<Player>[] = await playersWithRanking;
 
-			if (p1.s1_totalGames < p2.s1_totalGames) return 1;
-			if (p1.s1_totalGames > p2.s1_totalGames) return -1;
-
-			if (p1.name > p2.name) return 1;
-			if (p1.name < p2.name) return -1;
-
-			return 0;
-		});
-
-	const playersWithRanking: RankedPlayer[] = players.map((player, i) => {
-		const currRank = player.s1_ranked ? i + 1 : 0;
-		const dailyRanking = dailyRankedPlayers.findIndex((p) => p.id === player.id) + 1;
-		const weeklyRanking = weeklyRankedPlayers.findIndex((p) => p.id === player.id) + 1;
-		const monthlyRanking = monthlyRankedPlayers.findIndex((p) => p.id === player.id) + 1;
-
-		return {
-			...player,
-			dailyRank: dailyRanking - currRank,
-			weeklyRank: weeklyRanking - currRank,
-			monthlyRank: monthlyRanking - currRank
-		};
-	});
-
-	let result: Partial<Player>[] = playersWithRanking;
-
-	if (url.searchParams.has('simple')) {
-		result = players
-			.map((player) => ({
-				id: player.id,
-				name: player.name
-			}))
-			.toSorted((a, b) => (a.name < b.name ? -1 : 1));
+		if (url.searchParams.has('simple')) {
+			result = players
+				.map((player) => ({
+					id: player.id,
+					name: player.name
+				}))
+				.toSorted((a, b) => (a.name < b.name ? -1 : 1));
+		}
+		return new Response(JSON.stringify(result));
+	} catch (e) {
+		console.log('', e);
+		return new Response(JSON.stringify('error'));
 	}
-
-	return new Response(JSON.stringify(result));
 };
