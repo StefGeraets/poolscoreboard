b3c13b69b33ba9be352e936ee267a5e84957277f
Stef Geraets <stef.geraets@eventix.nl>
2024-04-26 16:29:41 +0200

    Fix: Footer season color

--- src/lib/components/PageFooter.svelte
+++ src/lib/components/PageFooter.svelte
@@ -86,7 +86,7 @@
 						<a
 							href={menuItem.url}
 							class="px-4 py-2 text-center uppercase border rounded border-gray-700/20 bg-gray-900/50"
-							class:bg-blue-800={menuItem.url.includes('season')}
+							class:bg-blue-700={menuItem.url.includes('season')}
 							target={menuItem.target}
 						>
 							{menuItem.title}
