92580741bdad19176ae24816fc0e5c18dfca0c8a
Stef Geraets <stef.geraets@eventix.nl>
2024-04-26 14:47:46 +0200

    Fix: Accent in menu

--- src/lib/components/PageFooter.svelte
+++ src/lib/components/PageFooter.svelte
@@ -80,7 +80,7 @@
 						<a
 							href={menuItem.url}
 							class="px-4 py-2 text-center uppercase border rounded border-gray-700/20 bg-gray-900/50"
-							class:bg-blue-700={i === 0}
+							class:bg-blue-800={menuItem.url.includes('season')}
 						>
 							{menuItem.title}
 						</a>
